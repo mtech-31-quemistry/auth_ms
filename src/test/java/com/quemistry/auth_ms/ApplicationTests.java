f2107fdaeaf2eadea27947995ce1c638b25e9b83
linxiaoxin <lin_xiaoxin@hotmail.com>
2024-06-03 23:10:12 +0800

    Initial Commit

---
+++ src/test/java/com/quemistry/auth_ms/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.quemistry.auth_ms;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
