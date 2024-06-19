c909e9b5e08083da1549ce5ff87e26a6aede9ab8
Raymond Htet <htetthurazaw@gmail.com>
2024-06-19 21:26:40 +0800

    remove printing all env variables

--- src/main/java/com/quemistry/auth_ms/Application.java
+++ src/main/java/com/quemistry/auth_ms/Application.java
@@ -3,20 +3,10 @@ package com.quemistry.auth_ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-
-		Map<String, String> env = System.getenv();
-		for (String envName : env.keySet()) {
-			System.out.format("%s=%s%n",
-					envName,
-					env.get(envName));
-		}
-
 		SpringApplication.run(Application.class, args);
 	}
 
