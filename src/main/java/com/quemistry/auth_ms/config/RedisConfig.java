4575468baddeea40a0ae05cefd21399d3116b61f
linxiaoxin <lin_xiaoxin@hotmail.com>
2024-07-03 23:29:01 +0800

    Change to lettuce connection with tls for redis

--- src/main/java/com/quemistry/auth_ms/config/RedisConfig.java
+++ src/main/java/com/quemistry/auth_ms/config/RedisConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -13,18 +15,20 @@ public class RedisConfig {
 
    @Bean
     public RedisConnectionFactory lettuceConnectionFactory(){
-       return new LettuceConnectionFactory((new RedisStandaloneConfiguration()));
+       var clientConfig = LettuceClientConfiguration.builder().useSsl().build();
+       return new LettuceConnectionFactory((new RedisStandaloneConfiguration()),clientConfig);
    }
 
    @Bean
     public RedisConnectionFactory jedisConnectionFactory(){
-       return new JedisConnectionFactory(new RedisStandaloneConfiguration());
+       var clientConfig = JedisClientConfiguration.builder().useSsl().build();
+       return new JedisConnectionFactory(new RedisStandaloneConfiguration(),clientConfig);
    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(lettuceConnectionFactory());
         return template;
     }
 }
