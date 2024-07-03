56e5ad54639d4c768f2a73c50072d69308f2be84
linxiaoxin <lin_xiaoxin@hotmail.com>
2024-07-03 23:59:08 +0800

    Add redis port and host when initiating connection

--- src/main/java/com/quemistry/auth_ms/config/RedisConfig.java
+++ src/main/java/com/quemistry/auth_ms/config/RedisConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -19,11 +19,11 @@ public class RedisConfig {
        return new LettuceConnectionFactory((new RedisStandaloneConfiguration()),clientConfig);
    }
 
-   @Bean
-    public RedisConnectionFactory jedisConnectionFactory(){
-       var clientConfig = JedisClientConfiguration.builder().useSsl().build();
-       return new JedisConnectionFactory(new RedisStandaloneConfiguration(),clientConfig);
-   }
+   //@Bean
+   // public RedisConnectionFactory jedisConnectionFactory(){
+  //     var clientConfig = JedisClientConfiguration.builder().useSsl().build();
+  //     return new JedisConnectionFactory(new RedisStandaloneConfiguration(),clientConfig);
+  // }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(){
