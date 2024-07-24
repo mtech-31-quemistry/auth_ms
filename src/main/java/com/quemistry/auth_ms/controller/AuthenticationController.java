c4e59ba440ba681501f9ed9368ea466e714cc29f
linxiaoxin <lin_xiaoxin@hotmail.com>
2024-07-24 14:50:17 +0800

    defined cookie domain name

--- src/main/java/com/quemistry/auth_ms/controller/AuthenticationController.java
+++ src/main/java/com/quemistry/auth_ms/controller/AuthenticationController.java
@@ -44,7 +44,11 @@ public class AuthenticationController {
 
         //create cookie and return code with cookie session
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Set-Cookie", String.format("%s=%s; Max-Age=%s; Path=/; HttpOnly;", COOKIE_NAME, userProfile.getSessionId(),sessionTimeout));
+        headers.add("Set-Cookie", String.format("%s=%s; Max-Age=%s; Domain=%s; Path=/; HttpOnly;"
+                                                            , COOKIE_NAME
+                                                            , userProfile.getSessionId()
+                                                            ,sessionTimeout
+                                                            , "dkraju438qs82.cloudfront.net"));
 
          return ResponseEntity.status(HttpStatus.OK).headers(headers).body(userProfile);
     }
@@ -54,7 +58,7 @@ public class AuthenticationController {
         authenticationService.signOut(cookie, signOutRequest.getClientId());
         //expire cookie to remove from session
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Set-Cookie", String.format("%s=%s; Max-Age=0; Path=/; HttpOnly;", COOKIE_NAME,""));
+        headers.add("Set-Cookie", String.format("%s=%s; Max-Age=0; Domain=%s; Path=/; HttpOnly;", COOKIE_NAME,"", "dkraju438qs82.cloudfront.net"));
 
         return ResponseEntity.status(HttpStatus.OK).headers(headers).body(null);
     }
