34ecb0c9f5b5918a3cdba6c92982ba7c2bd6a64a
ernestjx <73307945+ernestjx@users.noreply.github.com>
2024-09-12 15:55:45 +0800

    added student/accept-invitation, tutor/profile, student/profile apis

--- src/main/resources/db/migration/V1.8__add_user_api_resources.sql
+++ src/main/resources/db/migration/V1.8__add_user_api_resources.sql
@@ -1,6 +1,7 @@
 do
 $$
 DECLARE
+    get_student_profile integer;
     get_tutor_profile integer;
     post_tutor_profile integer;
     accept_invite integer;
@@ -9,6 +10,8 @@ DECLARE
 BEGIN
 
 -- Api Resource
+INSERT INTO qms_auth.api_resource(method, path)
+VALUES( 'GET', '/student/profile');
 
 INSERT INTO qms_auth.api_resource(method, path)
 VALUES( 'GET', '/users/tutor/profile');
@@ -20,6 +23,7 @@ INSERT INTO qms_auth.api_resource(method, path)
 VALUES( 'POST', '/student/accept-invitation');
 
 -- Grants
+SELECT id into get_student_profile FROM qms_auth.api_resource WHERE path = '/student/profile' and method = 'GET';
 SELECT id into get_tutor_profile FROM qms_auth.api_resource WHERE path = '/users/tutor/profile' and method = 'GET';
 SELECT id into post_tutor_profile FROM qms_auth.api_resource WHERE path = '/users/tutor/profile' and method = 'POST';
 SELECT id into accept_invite FROM qms_auth.api_resource WHERE path = '/student/accept-invitation' and method = 'POST';
@@ -28,6 +32,8 @@ select id into tutor FROM qms_auth.role where name = 'tutor';
 select id into student FROM qms_auth.role where name = 'student';
 
 /*generate question from ai*/
+INSERT INTO qms_auth.role_api_resource(resource_id, role_id)
+VALUES(get_student_profile, student);
 INSERT INTO qms_auth.role_api_resource(resource_id, role_id)
 VALUES( get_tutor_profile, tutor);
 INSERT INTO qms_auth.role_api_resource(resource_id, role_id)
